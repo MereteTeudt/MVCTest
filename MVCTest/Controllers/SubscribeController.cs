fa64b9bc839fffec9374352f3f799c8227bd02c4
MereteTeudt <mere0312@edu.campusvejle.dk>
2018-11-12 15:00:31 +0100

    comment

---
+++ MVCTest/Controllers/SubscribeController.cs
@@ -0,0 +1,30 @@
+﻿using MVCTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTest.Controllers
+{
+    public class SubscribeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Subscribe(SubscribeModel subscribeModel)
+        {
+            ResponseModel responseModel = new ResponseModel();
+            if (ModelState.IsValid)
+            {
+                responseModel.Success = true;
+                responseModel.ResponseMessage = "You've been subscribed.";
+            }
+
+            return View("Subscribe", responseModel);
+        }
+    }
+}
